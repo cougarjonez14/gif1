a8be8de5db383ebae701d467d1ae7968bd785fff
stationAstral <cougarjonez@gmail.com>
2023-11-03 23:23:44 -0700

    Initial Commit

---
+++ gif1/gif1App.swift
@@ -0,0 +1,17 @@
+//
+//  gif1App.swift
+//  gif1
+//
+//  Created by stationAstral on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct gif1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
